805f6eaa234b582f8db10e7be6b38ccf178e0e05
Adam Blevins <blevinspercussion@gmail.com>
2023-03-22 15:44:40 -0400

    Basic component structure in place

--- App.js
+++ App.js
@@ -1,6 +1,24 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
+import Switch from "./components/Switch";
+import GenerateButton from "./components/GenerateButton";
+
 export default function App() {
-  return <View></View>;
+  return (
+    <View style={styles.appContainer}>
+      <Text style={styles.title}>Password Generator</Text>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  appContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 25,
+  },
+});
---
+++ components/GenerateButton.jsx
@@ -0,0 +1,7 @@
+import { StyleSheet } from "react-native";
+
+export default function GenerateButton() {
+  return <View></View>;
+}
+
+const styles = StyleSheet.create({});

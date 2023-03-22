e7616a6063e5a25e83f2d18456b7c28f94a9aa74
Adam Blevins <blevinspercussion@gmail.com>
2023-03-22 15:51:44 -0400

    State and other global variables in place

--- App.js
+++ App.js
@@ -5,9 +5,28 @@ import Switch from "./components/Switch";
 import GenerateButton from "./components/GenerateButton";
 
 export default function App() {
+  const [password, setPassword] = useState("");
+  const [characterSwitches, setCharacterSwitches] = useState({
+    capital: false,
+    lowercase: false,
+    numbers: false,
+    special: false,
+  });
+  const [characters, setCharacters] = useState([]);
+
+  const capitalLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+  const lowercaseLetters = "abcdefghijklmnopqrstuvwxyz".split("");
+  const numbers = "1234567890".split("");
+  const special = "!@#$%^&*(){}/=?+,.<>".split("");
+
   return (
     <View style={styles.appContainer}>
       <Text style={styles.title}>Password Generator</Text>
+      <Switch />
+      <Switch />
+      <Switch />
+      <Switch />
+      <GenerateButton />
     </View>
   );
 }
